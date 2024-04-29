e04f9acf57bb1ef4f1b12fbefac340bff175f584
Edmore Moyo <etmoyo@gmail.com>
2024-04-29 11:21:52 -0400

    integrate with queue

--- fargate/compute-node-provisioner/terraform/infrastructure/compute-gateway-lambda/lambda_function.py
+++ fargate/compute-node-provisioner/terraform/infrastructure/compute-gateway-lambda/lambda_function.py
@@ -1,5 +1,7 @@
+from boto3 import client as boto3_client
 import json
 import base64
+import os
 
 def lambda_handler(event, context):
     if event['isBase64Encoded'] == True:
@@ -8,9 +10,14 @@ def lambda_handler(event, context):
         event['isBase64Encoded'] = False
     json_body = json.loads(event['body'])
     integration_id = json_body['integrationId']
+    sqs_url = os.environ['SQS_URL']
+
+    message = {"integrationId": integration_id}
+    sqs = boto3_client('sqs')
+    response = sqs.send_message(QueueUrl=sqs_url, MessageBody=json.dumps(message))
     
     print("Workflow started for ", integration_id)
     return {
         'statusCode': 202,
-        'body': json.dumps(integration_id)
+        'body': json.dumps(str(response))
     }
