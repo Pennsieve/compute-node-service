545dd340d0a120933eac716429a2bc737deab21c
Edmore Moyo <etmoyo@gmail.com>
2024-08-13 12:05:36 -0400

    fixes test

--- fargate/compute-node-provisioner/provisioner/provisioner_test.go
+++ fargate/compute-node-provisioner/provisioner/provisioner_test.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/service/iam"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
-	aws "github.com/pennsieve/compute-node-service/compute-node-provisioner/provisioner/aws"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	provisioner "github.com/pennsieve/compute-node-service/compute-node-provisioner/provisioner/aws"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAWSProvisioner(t *testing.T) {
-	provisioner := aws.NewAWSProvisioner(&iam.Client{}, &sts.Client{}, "someAccountId", "UNKNOWN_ACTION", "dev")
+	provisioner := provisioner.NewAWSProvisioner(aws.Config{}, "someAccountId", "UNKNOWN_ACTION", "dev")
 	err := provisioner.Run(context.Background())
 	assert.Equal(t, "action not supported: UNKNOWN_ACTION", err.Error())
 }
