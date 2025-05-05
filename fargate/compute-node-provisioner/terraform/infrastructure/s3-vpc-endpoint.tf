ae591beb30ca3daa9ce29620f933f197890f5281
Edmore Moyo <etmoyo@gmail.com>
2025-05-05 11:44:50 -0400

    temporarily remove gateway endpoint

--- fargate/compute-node-provisioner/terraform/infrastructure/s3-vpc-endpoint.tf
+++ fargate/compute-node-provisioner/terraform/infrastructure/s3-vpc-endpoint.tf
@@ -1,17 +1,17 @@
-# Retrieve all route tables associated with the default VPC
-data "aws_route_tables" "default_vpc_route_tables" {
-  vpc_id = aws_default_vpc.default.id
-}
+# # Retrieve all route tables associated with the default VPC
+# data "aws_route_tables" "default_vpc_route_tables" {
+#   vpc_id = aws_default_vpc.default.id
+# }
 
-# adds S3 VPC endpoint
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = aws_default_vpc.default.id
-  service_name = "com.amazonaws.${var.region}.s3"
-  route_table_ids   = data.aws_route_tables.default_vpc_route_tables.ids
+# # adds S3 VPC endpoint
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id       = aws_default_vpc.default.id
+#   service_name = "com.amazonaws.${var.region}.s3"
+#   route_table_ids   = data.aws_route_tables.default_vpc_route_tables.ids
 
-  tags = {
-    Environment = "${var.env}"
-    Region = "${var.region}"
-    Name = "${var.region} S3 VPC endpoint"
-  }
-}
+#   tags = {
+#     Environment = "${var.env}"
+#     Region = "${var.region}"
+#     Name = "${var.region} S3 VPC endpoint"
+#   }
+# }
